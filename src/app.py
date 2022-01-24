dcf90d117b24b604396dcd34d998f1a28f9f58db
Pamira Singtoroj <pamira.st@gmail.com>
2022-01-23 19:23:06 -0800

    Homepage anchors to dashboard & no code pages in HTML. HTML is pxcode generated (ignore)

--- src/app.py
+++ src/app.py
@@ -1,12 +1,72 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('Homepage.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('Dashboard.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/nocode')
+def no_code_homepage():
+    return render_template('Nocode.html')
+
+# @app.route('/', methods=['GET'])
+# def query_records():
+#     name = request.args.get('name')
+#     print(name)
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#         for record in records:
+#             if record['name'] == name:
+#                 return jsonify(record)
+#         return jsonify({'error': 'data not found'})
+
+# @app.route('/', methods=['PUT'])
+# def create_record():
+#     record = json.loads(request.data)
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#     if not data:
+#         records = [record]
+#     else:
+#         records = json.loads(data)
+#         records.append(record)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(records, indent=2))
+#     return jsonify(record)
+
+# @app.route('/', methods=['POST'])
+# def update_record():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#     for r in records:
+#         if r['name'] == record['name']:
+#             r['email'] = record['email']
+#         new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
+    
+# @app.route('/', methods=['DELETE'])
+# def delete_record():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#         for r in records:
+#             if r['name'] == record['name']:
+#                 continue
+#             new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
+
+app.run(debug=True)
