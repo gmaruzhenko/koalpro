509bb391c928a543d4971109aecdafd4e215f8a3
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-01-29 10:59:43 -0800

    react hooks working but not till after delay file name

--- frontend/src/components/nodes/CsvDataImportNode.js
+++ frontend/src/components/nodes/CsvDataImportNode.js
@@ -1,18 +1,22 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import  { Handle, Position } from 'react-flow-renderer';
 import '../../css/custom_nodes.css'
 
 
 const CsvDataImportNode = ({ data }) => {
-    const inputFile = useRef(null)
+    const inputFile = useRef(null);
+    const [fileName, setFileName] = useState("None Selected");//TODO hook this up to data
     const onButtonClick = () => {
         // `current` points to the mounted file input element
         inputFile.current.click();
-        console.log(inputFile.current.value)
     };
+    useEffect(() => {
+        setFileName(inputFile.current.value);
+    });
+
     return (
         <div className="csv_data_import_node">
-            <div>CSV Data Import From {data.import_source_name}</div>
+            <div>CSV Data Import From {fileName}</div>
             <Handle
                 type="source"
                 position={Position.Right}
