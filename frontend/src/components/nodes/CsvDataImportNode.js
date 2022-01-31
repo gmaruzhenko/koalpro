4d7015e38be1646977b57c4153c3b21d6c1aabdc
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-01-31 15:55:30 -0800

    adding column inputs vidsuals down

--- frontend/src/components/nodes/CsvDataImportNode.js
+++ frontend/src/components/nodes/CsvDataImportNode.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useRef, useState} from 'react';
 import  { Handle, Position,useStoreState  } from 'react-flow-renderer';
 import '../../css/custom_nodes.css'
+import '../../css/dnd.css'
+
 
 
 const CsvDataImportNode = ({ data }) => {
     const inputFile = useRef(null);
     const [fileName, setFileName] = useState(data.csv_name);//TODO hook this up to data and default "None Selected"
-    // const nodes = useStoreState((state) => state.nodes);
+    const [columnKeys, setColumnKeys] = useState(data.column_keys);
+    const [columnValues, setColumnValues] = useState(data.column_values);
 
     const onButtonClick = () => {
         // `current` points to the mounted file input element
@@ -28,8 +31,19 @@ const CsvDataImportNode = ({ data }) => {
                 id="a"
                 style={{ borderRadius: 0 }}
             />
-            <button onClick={onButtonClick}>Open file upload window</button>
-            <input type='file' id='file' ref={inputFile} onChange={handleUpdate} style={{display: 'none'}}/>
+            <div className="row-cols-1">
+                <div className="row">
+                    <button onClick={onButtonClick}>Open file upload window</button>
+                    <input type='file' id='file' ref={inputFile} onChange={handleUpdate} style={{display: 'none'}}/>
+
+                </div>
+                <div className="row">
+                <input className="text-input-field" type="text" value={columnKeys} placeholder={"enter column letter with company names"}/>
+                </div>
+                    <div className="row">
+                <input className="text-input-field" type="text" value={columnValues} placeholder={"enter column letter with associated data"}/>
+                    </div>
+            </div>
         </div>
     );
 };
