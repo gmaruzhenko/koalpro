cdf9a2ec17bf0c55c287a35ad3a599635f6e1749
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-02-24 17:58:46 -0800

    added grid rather than list

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { FixedSizeList as List } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeGrid as Grid } from 'react-window';
 import '../css/dnd.css';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 
 const Dashboard = () => {
-    const Row = ({ index, style }) => (
-        <div className={index % 2 ? "list-item-odd" : "list-item-even"} style={style}>
-            <label> Company name {index} </label>
-            <label> $$$  {index*100}</label>
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+    const updateWidthAndHeight = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    };
+    useEffect(() => {
+        window.addEventListener("resize", updateWidthAndHeight);
+        return () => window.removeEventListener("resize", updateWidthAndHeight);
+    });
+
+
+    const Cell = ({ columnIndex, rowIndex, style }) => (
+        <div style={style}>
+            Item {rowIndex},{columnIndex}
         </div>
     );
     return (
         <div className="dashboard">
-        <AutoSizer>
-            {({ height, width }) => (
-                <List
-                    className="List"
-                    height={height}
-                    itemCount={1000}
-                    itemSize={35}
-                    width={width}
-                >
-                    {Row}
-                </List>
-            )}
-        </AutoSizer>
+            <Grid
+                columnCount={3}
+                columnWidth={200}
+                height={height}
+                rowCount={1000}
+                rowHeight={35}
+                width={width}
+            >
+                {Cell}
+            </Grid>
         </div>
     );
 };
