7f8598528b2a1ab61522e1ba6d14732110a7aa13
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-03-04 15:49:54 -0800

    added a script to launch both back and frontend for demos

---
+++ launch_koalpro.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#cd frontend/ && yarn start; ls
+#(cd ./backend/ && source env/Scripts/activate && cd ./src && flask run) & (cd frontend/ && yarn start )
+#(trap 'kill 9' SIGINT; (cd ./backend/ && source env/Scripts/activate && cd ./src && flask run) &  (cd frontend/ && yarn start ))
+
+#Note that this doesnt kill the second process and you gotta manually do it
+
+(cd ./backend/ && source env/Scripts/activate && cd ./src && flask run) &  (cd frontend/ && yarn start )
