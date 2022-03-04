20864f854289a6ed4ab328cf931b78c5486bd551
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-03-04 13:29:46 -0800

    react visualized  working well

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,55 +1,244 @@
-import React, {useEffect, useState} from "react";
-import { FixedSizeList as List } from "react-window";
-import AutoSizer from "react-virtualized-auto-sizer";
-import { FixedSizeGrid as Grid } from 'react-window';
-import '../css/dnd.css';
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import { AutoSizer, Column, Table } from 'react-virtualized';
+
+const styles = (theme) => ({
+    flexContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        boxSizing: 'border-box',
+    },
+    table: {
+        // temporary right-to-left patch, waiting for
+        // https://github.com/bvaughn/react-virtualized/issues/454
+        '& .ReactVirtualized__Table__headerRow': {
+            ...(theme.direction === 'rtl' && {
+                paddingLeft: '0 !important',
+            }),
+            ...(theme.direction !== 'rtl' && {
+                paddingRight: undefined,
+            }),
+        },
+    },
+    tableRow: {
+        cursor: 'pointer',
+    },
+    tableRowHover: {
+        '&:hover': {
+            backgroundColor: theme.palette.grey[200],
+        },
+    },
+    tableCell: {
+        flex: 1,
+    },
+    noClick: {
+        cursor: 'initial',
+    },
+});
 
 
-const Dashboard = () => {
 
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
-    const updateWidthAndHeight = () => {
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight);
+
+class MuiVirtualizedTable extends React.PureComponent {
+    static defaultProps = {
+        headerHeight: 48,
+        rowHeight: 48,
     };
-    useEffect(() => {
-        window.addEventListener("resize", updateWidthAndHeight);
-        return () => window.removeEventListener("resize", updateWidthAndHeight);
-    });
 
+    getRowClassName = ({ index }) => {
+        const { classes, onRowClick } = this.props;
 
-    const Cell = ({ columnIndex, rowIndex, style }) => (
-        <div style={style}>
-            Item {rowIndex},{columnIndex}
-        </div>
-    );
-    return (
-        <div className="container">
-            <TableHead rowSpan={2}>
-                <TableRow >
-                    <TableCell>Desc</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="center">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="left">Carbs&nbsp;(g)</TableCell>
-                </TableRow>
-            </TableHead>
-            <Grid
-                columnCount={3}
-                columnWidth={200}
-                height={height}
-                rowCount={1000}
-                rowHeight={35}
-                width={width}
+        return clsx(classes.tableRow, classes.flexContainer, {
+            [classes.tableRowHover]: index !== -1 && onRowClick != null,
+        });
+    };
+
+    cellRenderer = ({ cellData, columnIndex }) => {
+        const { columns, classes, rowHeight, onRowClick } = this.props;
+        return (
+            <TableCell
+                component="div"
+                className={clsx(classes.tableCell, classes.flexContainer, {
+                    [classes.noClick]: onRowClick == null,
+                })}
+                variant="body"
+                style={{ height: rowHeight }}
+                align={
+                    (columnIndex != null && columns[columnIndex].numeric) || false
+                        ? 'right'
+                        : 'left'
+                }
             >
-                {Cell}
-            </Grid>
-        </div>
-    );
+                {cellData}
+            </TableCell>
+        );
+    };
+
+    headerRenderer = ({ label, columnIndex }) => {
+        const { headerHeight, columns, classes } = this.props;
+
+        return (
+            <TableCell
+                component="div"
+                className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+                variant="head"
+                style={{ height: headerHeight }}
+                align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+            >
+                <span>{label}</span>
+            </TableCell>
+        );
+    };
+
+    render() {
+        const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
+        return (
+            <AutoSizer>
+                {({ height, width }) => (
+                    <Table
+                        height={height}
+                        width={width}
+                        rowHeight={rowHeight}
+                        gridStyle={{
+                            direction: 'inherit',
+                        }}
+                        headerHeight={headerHeight}
+                        className={classes.table}
+                        {...tableProps}
+                        rowClassName={this.getRowClassName}
+                    >
+                        {columns.map(({ dataKey, ...other }, index) => {
+                            return (
+                                <Column
+                                    key={dataKey}
+                                    headerRenderer={(headerProps) =>
+                                        this.headerRenderer({
+                                            ...headerProps,
+                                            columnIndex: index,
+                                        })
+                                    }
+                                    className={classes.flexContainer}
+                                    cellRenderer={this.cellRenderer}
+                                    dataKey={dataKey}
+                                    {...other}
+                                />
+                            );
+                        })}
+                    </Table>
+                )}
+            </AutoSizer>
+        );
+    }
+}
+
+MuiVirtualizedTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+    columns: PropTypes.arrayOf(
+        PropTypes.shape({
+            dataKey: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            numeric: PropTypes.bool,
+            width: PropTypes.number.isRequired,
+        }),
+    ).isRequired,
+    headerHeight: PropTypes.number,
+    onRowClick: PropTypes.func,
+    rowHeight: PropTypes.number,
 };
-export default Dashboard
+
+const defaultTheme = createTheme();
+const VirtualizedTable = withStyles(styles, { defaultTheme })(MuiVirtualizedTable);
+
+// ---
+
+const sample = [
+    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+    ['Eclair', 262, 16.0, 24, 6.0],
+    ['Cupcake', 305, 3.7, 67, 4.3],
+    ['Gingerbread', 356, 16.0, 49, 3.9],
+];
+
+
+const sampledata = [
+    {
+        "companyID": "Apple",
+        "cross_sell_value": 1000,
+        "up_sell_value": 2000
+
+    },
+    {
+        "companyID": "Microsoft",
+        "cross_sell_value": 2000,
+        "up_sell_value": 5000
+
+    },
+    {
+        "companyID": "Tesla",
+        "cross_sell_value": 3000,
+        "up_sell_value": 1000
+
+    },
+    {
+        "companyID": "Google",
+        "cross_sell_value": 4000,
+        "up_sell_value": 8000
+
+    },
+    {
+        "companyID": "Urmom",
+        "cross_sell_value": 5000,
+        "up_sell_value": 69000
+
+    }
+];
+
+const rows = [...sampledata,...sampledata,...sampledata,...sampledata,...sampledata];
+
+
+// function createData(id, dessert, calories, fat, carbs, protein) {
+//     return { id, dessert, calories, fat, carbs, protein };
+// }
+//
+// const rows = [];
+//
+// for (let i = 0; i < 200; i += 1) {
+//     const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+//     rows.push(createData(i, ...randomSelection));
+// }
+
+export default function Dashboard() {
+    return (
+        <Paper style={{ height: 400, width: '100%' }}>
+            <VirtualizedTable
+                rowCount={rows.length}
+                rowGetter={({ index }) => rows[index]}
+                columns={[
+                    {
+                        width: 200,
+                        label: 'Company Name',
+                        dataKey: 'companyID',
+                    },
+                    {
+                        width: 200,
+                        label: 'Cross-Sell Value',
+                        dataKey: 'cross_sell_value',
+                        numeric: true,
+                    },
+                    {
+                        width: 200,
+                        label: 'Up-Sell Value',
+                        dataKey: 'up_sell_value',
+                        numeric: true,
+                    },
+
+                ]}
+            />
+        </Paper>
+    );
+}
