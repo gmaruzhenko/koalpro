485bc43df6ffe04d867f0314a4c6d23fe54ec02f
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-03-22 13:16:39 -0700

    hardcoded libarary of products and node done

--- frontend/src/components/nodes/ProductNode.js
+++ frontend/src/components/nodes/ProductNode.js
@@ -8,36 +8,62 @@ import defaultStartNodes from "../defaultStartNodes";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 
-const onEdgeClick = (evt, id,reactFlowInstance) => {
+const onEdgeClick = (evt, id, reactFlowInstance) => {
     evt.stopPropagation();
-    let after_edges = reactFlowInstance.getNodes().filter(node => node.id!== id);
-    if (reactFlowInstance.getNodes().length===1){
+    let after_edges = reactFlowInstance.getNodes().filter(node => node.id !== id);
+    if (reactFlowInstance.getNodes().length === 1) {
         reactFlowInstance.setNodes(defaultStartNodes);
 
-    }else{
+    } else {
         reactFlowInstance.setNodes(after_edges);
 
     }
 
 };
 
-const ProductNode = ({data,id}) => {
-    const [product, setProduct] = useState(data.product||2);
+const productLibrary = {
+    "product": [
+        {
+            "unit_price": 2,
+            "product_name": "Puck"
+        },
+        {
+            "unit_price": 30,
+            "product_name": "Helmet"
+        },
+        {
+            "unit_price": 10,
+            "product_name": "Stick"
+        },
+
+    ]
+
+};
+
+const ProductNode = ({data, id}) => {
+    const [productPrice, setProductPrice] = useState(data.product.unit_price || ''); //default to a state to ensure controlled
     const handleChange = (event) => {
-        setProduct(event.target.value);
+        setProductPrice(event.target.value);
     };
+    useEffect(() => {
+            // data.product.unit_price = productPrice;
+            data.product = productLibrary.product.filter(unit => unit.unit_price === productPrice);
+        },
+        [productPrice]
+    );
     const reactFlowInstance = useReactFlow();
 
     return (
         <div className="product-node">
-            <IconButton aria-label="delete" style={{float:'right',vertical_align: 'top',padding:'0px'}} size="small" onClick={(event) => onEdgeClick(event, id,reactFlowInstance)}>
-                <HighlightOffIcon fontSize="inherit" />
+            <IconButton aria-label="delete" style={{float: 'right', vertical_align: 'top', padding: '0px'}} size="small"
+                        onClick={(event) => onEdgeClick(event, id, reactFlowInstance)}>
+                <HighlightOffIcon fontSize="inherit"/>
             </IconButton>
-            <Handle type="target" id="input_1" position={Position.Left} style={{  top: '50%',borderRadius: 0 }} />
+            <Handle type="target" id="input_1" position={Position.Left} style={{top: '50%', borderRadius: 0}}/>
 
             <div>Product</div>
             <Select
-                value={product}
+                value={productPrice}
                 onChange={handleChange}
             >
                 <MenuItem value={2}>Pucks 2$/unit</MenuItem>
@@ -48,7 +74,7 @@ const ProductNode = ({data,id}) => {
                 type="source"
                 position={Position.Right}
                 id="a"
-                style={{ borderRadius: 0 }}
+                style={{borderRadius: 0}}
             />
 
         </div>
