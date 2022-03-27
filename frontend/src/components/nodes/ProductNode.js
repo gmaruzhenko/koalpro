33ddd3d1ca4ff6c46e82f329b84b5ae637671a8e
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-03-26 17:27:53 -0700

    changed wording on product node

--- frontend/src/components/nodes/ProductNode.js
+++ frontend/src/components/nodes/ProductNode.js
@@ -11,7 +11,7 @@ import deleteNodeHelper from "../deleteNodeHelper";
 
 const onEdgeClick = (evt, id, reactFlowInstance) => {
     evt.stopPropagation();
-    deleteNodeHelper(reactFlowInstance,id);
+    deleteNodeHelper(reactFlowInstance, id);
 
 };
 
@@ -36,10 +36,11 @@ const productLibrary = {
 
 const ProductNode = ({data, id}) => {
     // const price = data.product[0].unit_price
-    if (data.product === undefined){
+    if (data.product === undefined) {
         data.product = productLibrary.product;
     }
     const [productPrice, setProductPrice] = useState(data.product[0].unit_price); //default to a state to ensure controlled
+    // const [productName, setProductName] = useState(productLibrary.product.); //default to a state to ensure controlled
     const handleChange = (event) => {
         setProductPrice(event.target.value);
     };
@@ -60,14 +61,16 @@ const ProductNode = ({data, id}) => {
             <Handle type="target" id="input_1" position={Position.Left} style={{top: '50%', borderRadius: 0}}/>
 
             <div>Product</div>
-            <Select
-                value={productPrice}
-                onChange={handleChange}
-            >
-                <MenuItem value={2}>Pucks 2$/unit</MenuItem>
-                <MenuItem value={30}>Helmet 30$/unit</MenuItem>
-                <MenuItem value={10}>Sticks 10$/unit</MenuItem>
-            </Select>
+            <div className="row-cols-1">
+                <Select
+                    value={productPrice}
+                    onChange={handleChange}
+                >
+                    <MenuItem value={2}>5 Pucks per Stick at 2$/unit</MenuItem>
+                    <MenuItem value={30}>1 Helmet per Stick at 30$/unit</MenuItem>
+                    <MenuItem value={10}>1 Sticks per Stick at 10$/unit</MenuItem>
+                </Select>
+            </div>
             <Handle
                 type="source"
                 position={Position.Right}
