7a186f8a204d8efd4e741b88104262828fcee8f3
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2022-03-28 13:16:21 -0700

    added generation script

---
+++ backend/src/generate_new_csv.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = np.random.randint(100,50000,size=(499,1))
+df = pd.DataFrame(data, columns=['Number of Sticks Sold'])
+csv_name ="../../resources/Region 2 Alberta Current purchased sticks.csv"
+df_old = pd.read_csv(csv_name)
+df_old['Number of Sticks Sold'] = df['Number of Sticks Sold']
+df_old.to_csv(csv_name)
+print(df_old)
